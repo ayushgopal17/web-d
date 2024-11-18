539b644e791b3bd7551ee1f737c61eb67caf08b6
Ayush gopal <ayushgopal17@gmail.com>
2024-11-18 20:56:03 +0530

    javascreep basics

---
+++ class 1/main.js
@@ -0,0 +1,13 @@
+let firstname ="ayush";
+let age = 20;
+let ismarried = false;
+
+
+
+if (ismarried == true)
+{
+    console.log(firstname+ " is married")
+}
+else{
+    console.log(firstname + " is not married")
+}
