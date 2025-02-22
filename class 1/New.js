384ada5202a76d9c6dccf2c07ce7e974287173d0
Ayush gopal <ayushgopal17@gmail.com>
2025-02-22 14:28:00 +0530

    for loop printing sum of 1000

--- class 1/New.js
+++ class 1/New.js
@@ -2,14 +2,14 @@ let Name = "ayush gopal";
 let age=20;
 let ismarried = false;
 
-//console.log("this person name is "+ Name + " and His age is "+age)
+console.log("this person name is "+ Name + " and His age is "+age)
 
-if(ismarried == true)
-{
-    console.log(Name+" is married ")
-}
+// if(ismarried == true)
+// {
+//     console.log(Name+" is married ")
+// }
 
-else
-    { 
-        console.log(Name+" is not marries ")
-    }
+// else
+//     { 
+//         console.log(Name+" is not marries ")
+//     }
